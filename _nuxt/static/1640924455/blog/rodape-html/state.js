48276e7ada0056eb019f36fec836846ae3a79fa0
fupicat <fupicat@users.noreply.github.com>
2021-12-31 19:00:12 +0000

    deploy: f55e70e5675bb26bc2688b7391ff0fb08fbcb222

--- _nuxt/static/1640924455/blog/rodape-html/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640924455",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fblog\u002Frodape-html",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62bb6741"}},__i18n:{langs:{}}}}(null,false));
