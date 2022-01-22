f5293573e8c83f86861d0e5b11bcbdab50a4d018
fupicat <fupicat@users.noreply.github.com>
2022-01-22 23:40:46 +0000

    deploy: 0452930f030753bc1e2f04cf815c99f1f3924721

--- _nuxt/static/1642649384/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642649384",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"faab4810"}},__i18n:{langs:{}}}}(null,false,"\u002F"));
