856c98df63fb2b19f6445f302f28b4c88a29d8d3
fupicat <fupicat@users.noreply.github.com>
2022-01-26 01:55:20 +0000

    deploy: be71347cb1c40933d45869251fe1797f7eb1de24

--- _nuxt/static/1643003047/jogos/follow/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643003047",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fjogos\u002Ffollow",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0b43f4eb"}},__i18n:{langs:{}}}}(null,false));
---
+++ _nuxt/static/1643162113/jogos/follow/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643162113",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fjogos\u002Ffollow",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9d29feac"}},__i18n:{langs:{}}}}(null,false));
