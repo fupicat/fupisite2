27b43199d4e94a36a770eed6ce9310962c727735
Fupi <98890458+GabZanMacaw@users.noreply.github.com>
2022-02-13 00:25:25 -0300

    cms test 5

--- static/js/identity.js
+++ static/js/identity.js
@@ -1,12 +1,7 @@
-console.log("entrou");
 if (window.netlifyIdentity) {
-  console.log("tem identity");
   window.netlifyIdentity.on("init", (user) => {
-    console.log("iniciou");
     if (!user) {
-      console.log("não é usuario");
       window.netlifyIdentity.on("login", () => {
-        console.log("fez login");
         document.location.href = "/admin/";
       });
     }
