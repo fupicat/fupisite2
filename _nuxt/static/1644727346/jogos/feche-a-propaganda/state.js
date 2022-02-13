e4f4dbc608eda189f918de8be78a35eef39b7927
fupicat <fupicat@users.noreply.github.com>
2022-02-13 04:42:34 +0000

    deploy: daea1497363ce034478438d667b340d7e3d711be

---
+++ _nuxt/static/1644727346/jogos/feche-a-propaganda/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644727346",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fjogos\u002Ffeche-a-propaganda",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41e9238a"}},__i18n:{langs:{}}}}(null,false));
---
+++ _nuxt/static/1644727346/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644727346",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41e9238a"}},__i18n:{langs:{}}}}(null,false,"\u002F"));
