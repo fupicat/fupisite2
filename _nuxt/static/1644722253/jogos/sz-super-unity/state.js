413620b3d4ad3bade65bcf8105743009e95f8a55
fupicat <fupicat@users.noreply.github.com>
2022-02-13 03:17:41 +0000

    deploy: 55e5bd844cf09665c35d944c78a931e793ecbcfa

---
+++ _nuxt/static/1644722253/jogos/sz-super-unity/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644722253",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fjogos\u002Fsz-super-unity",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3bbabeca"}},__i18n:{langs:{}}}}(null,false));
