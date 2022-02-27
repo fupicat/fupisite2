cceb1cc35006c6db1454c4f059edea1e88674c55
fupicat <fupicat@users.noreply.github.com>
2022-02-27 19:17:25 +0000

    deploy: 9bcaa55d87a481e9ba6cba6ff6deec6c6337ebab

--- _nuxt/static/1644727346/jogos/blind-boom/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644727346",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fjogos\u002Fblind-boom",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41e9238a"}},__i18n:{langs:{}}}}(null,false));
