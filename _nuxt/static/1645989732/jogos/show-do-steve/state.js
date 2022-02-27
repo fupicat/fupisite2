e52e2a978e1148aad364cae5a2ff8c9334bc387a
fupicat <fupicat@users.noreply.github.com>
2022-02-27 19:22:20 +0000

    deploy: c6f2e43e33f3819fc8c35c9d199f73c2b74b6728

---
+++ _nuxt/static/1645989732/jogos/show-do-steve/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645989732",layout:"default",error:a,state:{dark:{dark:b},nfe:{nfe:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fjogos\u002Fshow-do-steve",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b69645f1"}},__i18n:{langs:{}}}}(null,false));
